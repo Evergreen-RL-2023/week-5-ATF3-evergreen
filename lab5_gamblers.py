72f8feea1d3dc72196060cc3225d10e2d2592721
ATF3-evergreen <atf3@evergreen.edu>
2023-05-08 04:50:35 +0000

    some comments, error still in index

--- lab5_gamblers.py
+++ lab5_gamblers.py
@@ -26,18 +26,18 @@ class Agent(object):
 
 class Environment(object):
     def __init__(self, p_heads=0.4):
-        self.p_heads = p_heads
+        self.p_heads = p_heads # probability of winning
 
 # TODO get the indexing right, next_state can't return > 99
-    def step(self, state, action):
+    def step(self, state, action): # 
         if rnd.random() <= self.p_heads:
-            next_state = state + action
+            next_state = state + action  # winning action (update wallet balance)
         else:
-            next_state = state - action
+            next_state = state - action  # losing action  (update wallet balance)
 
-        if next_state >= 99:  # win
+        if next_state == 100:  # win
             reward = 1
-             # next_state = 0  make next state value = 0  TODO
+            # TODO solve this OOB error!
             agent.values[next_state] = 0
             
         elif next_state == 0:  # lose 
@@ -50,12 +50,12 @@ class Environment(object):
 
 def value_iteration(agent, environment, num_iterations):
     for _ in range(num_iterations):
-        new_values = np.zeros(N)  # check for index oob
-        for state in range(1, N):
+        new_values = np.zeros(N + 1)  # check for index oob
+        for state in range(1, N):  #  iterate through states, except terminal states 0 and 100
             values = []
-            for action in range(1, min(state, N - state)+ 1):
+            for action in range(1, min(state, N - state) + 1): # From 1 to wallet balance 
                 next_state, reward = environment.step(state, action)
-                values.append(reward + agent.values[next_state - 1] * gamma)
+                values.append(reward + agent.values[next_state - 1] * gamma)  # calculate value
             new_values[state - 1] = np.mean(values)  # Uniform random policy
         agent.values = new_values
 
